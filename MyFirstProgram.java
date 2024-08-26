bd61513d219ed7621c3644b20e93c763bdeed18b
syedjafarmoinuddin786 <jafarmoinuddin2005@gmail.com>
2024-08-26 14:43:35 +0530

    cloning

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,5 +1,6 @@
 public class MyFirstProgram{
         public static void main(String[] args){
             System.out.print("Assalaam , welcome to git ws");
+            System.out.println("HELO WELCOME TO GITHUB WS");
         }
 }
